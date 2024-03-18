bfffc2b8406b48da732ca2e6687b221bb995a63a
JuanFDJ73 <vavodri@gmail.com>
2024-03-18 14:34:12 -0500

    Conexion base de datos(Prueba), Falta: Modularizar, Crear una estructura escalable, permanencia de mensajes(Via server o Local)

---
+++ database/localStorage.js
@@ -0,0 +1,9 @@
+// function saveData() {
+//     const inputValue = document.getElementById('myInput').value;
+//     localStorage.setItem('myData', inputValue);
+// }
+
+// function getData() {
+//     const savedData = localStorage.getItem('myData');
+//     document.getElementById('displayData').textContent = savedData || 'No data saved';
+// }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
 import express from "express";
 import logger from "morgan";
+import dotenv from "dotenv";
 
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 const port = process.env.PORT || 3000;
 
 const app = express()
 const server = createServer(app);
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery: {
+        timeout: 10000,
+        maxAttempts: 10
+    }
+})
+
+dotenv.config();
 
 //Ruta de las imagenes estaticas
 app.use(express.static('public'));
 
 //Conexion con el websocket
 io.on('connection', (socket) => {
+    /////////////////////////////////////////////
+    // Realizar el guardado en el localStorage //
+    /////////////////////////////////////////////
     console.log('a user has connected')
 
     socket.on('disconnect', () => {
@@ -22,17 +36,48 @@ io.on('connection', (socket) => {
     })
 
     socket.on('chat message', (message) => {
+        // Guardar el mensaje en Firestore
+        console.log(message)
+        const mensajesCollection = collection(db, 'mensajes');
+
+        const nuevoMensaje = {
+            texto: message,
+            usuario: "message.usuario",
+            timestamp: new Date()
+        };
+
+        addDoc(mensajesCollection, nuevoMensaje)
+            .then((docRef) => {
+                console.log('Mensaje guardado en Firestore con ID: ', docRef.id);
+            })
+            .catch((error) => {
+                console.error('Error al guardar mensaje en Firestore: ', error);
+            });
         io.emit('chat message', message)
     })
-    
-});
 
+});
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html');
 })
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
 
 server.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
