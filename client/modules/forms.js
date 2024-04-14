4bfc61e35a72d29eabd9cc941c8251bf7be84eaf
JuanFDJ73 <vavodri@gmail.com>
2024-04-14 14:56:22 -0500

    Modulos del sidebar, estructura basica (posibles cambios a futuro). Falta: Funciones, complementos, etc.

--- client/modules/forms.js
+++ client/modules/forms.js
@@ -29,3 +29,115 @@ export function handleSubmit(socket) {
         console.error('Ocurrió un error:', error);
     }
 }
+
+export function createModalStructure(className, title) {
+    // Crear el div del modal
+    const modalDiv = document.createElement('div');
+    modalDiv.id = 'modal';
+    modalDiv.className = 'modal';
+    modalDiv.style.display = 'flex';
+
+    // Crear el contenido del modal
+    const modalContentDiv = document.createElement('div');
+    modalContentDiv.className = className;
+
+    // Crear el botón de cerrar
+    const closeButton = document.createElement('span');
+    closeButton.className = 'close-button';
+    closeButton.innerHTML = '&times;';
+
+    closeButton.addEventListener('click', function() {
+        removeModal();
+    });
+
+    // Crear el título del modal
+    const titleH2 = document.createElement('h2');
+    titleH2.innerText = title;
+
+    // Agregar el botón de cerrar y el título al contenido del modal
+    modalContentDiv.appendChild(closeButton);
+    modalContentDiv.appendChild(titleH2);
+
+    // Agregar el contenido del modal al div del modal
+    modalDiv.appendChild(modalContentDiv);
+
+    // Agregar el div del modal al body del documento
+    document.body.appendChild(modalDiv);
+
+    //Cierra el modal al hacer clic fuera de él
+    window.onclick = function(event) {
+    
+        if (event.target == modalDiv) {
+            removeModal();
+        }
+    }
+}
+
+export function createFormAddContact() {
+    // Obtener el contenido del modal
+    const modalContentDiv = document.querySelector('.modal-add-contact');
+
+    // Crear el formulario
+    const form = document.createElement('form');
+    form.id = 'contactForm';
+
+    // Crear el div del campo de entrada
+    const formGroup1 = document.createElement('div');
+    formGroup1.className = 'form-group';
+
+    // Crear la etiqueta del campo de entrada
+    const label1 = document.createElement('label');
+    label1.setAttribute('for', 'formAddContact');
+    label1.innerText = 'Id del usuario:';
+
+    // Crear el campo de entrada
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'formAddContact';
+    input.placeholder = 'Escribe el Id';
+
+    // Agregar la etiqueta y el campo de entrada al div del campo de entrada
+    formGroup1.appendChild(label1);
+    formGroup1.appendChild(input);
+
+    // Crear el div del botón
+    const formGroup2 = document.createElement('div');
+    formGroup2.className = 'form-group';
+
+    // Crear el botón de enviar
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.className = 'submit-button';
+    submitButton.innerText = 'Enviar';
+
+    // Agregar el botón al div del botón
+    formGroup2.appendChild(submitButton);
+
+    // Agregar los elementos al formulario
+    form.appendChild(formGroup1);
+    form.appendChild(formGroup2);
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        console.log('Formulario enviado');
+        //NOTA: Hacer Validaciones, posteriormente cerrar el modal
+        input.value = ''
+        removeModal();
+
+    });
+
+    // Agregar el formulario al contenido del modal
+    modalContentDiv.appendChild(form);
+}
+
+export function removeModal() {
+    // Obtener el div del modal
+    const modalDiv = document.getElementById('modal');
+
+    // Verificar si el modal existe antes de intentar eliminarlo
+    if (modalDiv) {
+        // Eliminar el div del modal
+        modalDiv.parentNode.removeChild(modalDiv);
+    }
+}
--- client/modules/main.js
+++ client/modules/main.js
@@ -2,6 +2,7 @@ import { displayMessage, userIdVisibilyOff, switchUserIdVisibility } from '/ui.j
 import { setCookieUser, cleanCookieContact, setContacts } from '/api.js';
 import { setupSocketListeners, socket } from '/socket.js';
 import { scrollToBottom, handleEscapeKey } from '/utils.js';
+import { createModalStructure, createFormAddContact, removeModal} from './forms.js';
 
 function initChatApp() {
     window.onload = function() {
@@ -18,36 +19,38 @@ function initChatApp() {
     }
     document.addEventListener('keydown', handleEscapeKey);
     document.addEventListener('DOMContentLoaded', function() {
-        const button = document.querySelector('.addContact');
-        const modal = document.getElementById('modal');
-        const closeButton = document.querySelector('.close-button');
+        const button1 = document.querySelector('.userContainer');
+        const button2 = document.querySelector('.addContact');
+        const button3 = document.querySelector('.optionsContainer');
+        const button4 = document.querySelector('.helpContainer');
     
-        //Prueba: Luego se reestructurara el modal
-        button.addEventListener('click', function() {
-            modal.style.display = 'flex'; // Cambia de 'none' a 'flex' para mostrar el modal
-        });
-    
-        closeButton.addEventListener('click', function() {
-            modal.style.display = 'none'; // Ocultar el modal
+        button1.addEventListener('click', function() {
+            const className = "modal-content modal-user"
+            const title = "Perfil"
+            createModalStructure(className, title);
         });
 
-        document.getElementById('contactForm').addEventListener('submit', function(event) {
-            event.preventDefault(); 
-            
-            console.log('Formulario enviado');
-            //NOTA: Hacer Validaciones, posteriormente cerrar el modal
-            const input = document.getElementById('formAddContact');
-            input.value = ''
-            document.getElementById('modal').style.display = 'none'; // Ocultar el modal
+        button2.addEventListener('click', function() {
+            const className = "modal-content modal-add-contact"
+            const title = "Agregar Contacto"
+            createModalStructure(className, title);
+            createFormAddContact();
+        });
+
+        button3.addEventListener('click', function() {
+            const className = "modal-content modal-settings"
+            const title = "Configuraciones"
+            createModalStructure(className, title);
+            createFormAddContact();
+        });
+
+        button4.addEventListener('click', function() {
+            const className = "modal-content modal-help"
+            const title = "Ayuda"
+            createModalStructure(className, title);
+            createFormAddContact();
         });
         
-    
-        //Cierra el modal al hacer clic fuera de él
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = 'none';
-            }
-        }
     });    
 }
 
